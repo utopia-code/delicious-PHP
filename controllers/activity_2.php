cd49cd53e85b99be663e7ab0736d410ef0e18e61
utopia-code <utopiacode.developer@gmail.com>
2024-05-10 14:55:52 +0200

    PEC3: delete comments

--- controllers/activity_2.php
+++ controllers/activity_2.php
@@ -2,16 +2,4 @@
 
 $recipes = $app['database']->selectAll('receta_php');
 
-foreach ($recipes as $recipe) {
-
-    if ($recipe->id === 7) {
-        // var_dump(convertInMinutes($recipe->cooking_time));
-
-        // var_dump(str_replace('\r\n','<br/>', $recipe->ingredients));
-    }
-    
-}
-
-
-
 require 'views/activity_2.view.php';
